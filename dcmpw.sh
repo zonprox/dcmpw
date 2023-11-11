628db6a9444a7ccfe204ae3a169ff8ddcbffff77
Zon Harris <zonprox@users.noreply.github.com>
2023-11-11 14:54:28 +0700

    Update dcmpw.sh

--- dcmpw.sh
+++ dcmpw.sh
@@ -7,13 +7,10 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Set locale
-echo -e "${YELLOW}Setting locale...${NC}"
+# Set locale and timezone
+echo -e "${YELLOW}Setting locale and timezone...${NC}"
 sudo locale-gen en_US.UTF-8
 sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
-
-# Set timezone
-echo -e "${YELLOW}Setting timezone...${NC}"
 sudo timedatectl set-timezone Asia/Ho_Chi_Minh
 
 # Set nameserver
@@ -45,60 +42,8 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo 
 sudo apt update
 sudo apt install caddy -y
 
-# Prompt user for domain and email
+# Prompt user for domain
 read -p "$(echo -e ${YELLOW}"[?] Your domain (e.g., example.com): "${NC})" DOMAIN
-read -p "$(echo -e ${YELLOW}"[?] Your email for SSL certificate: "${NC})" EMAIL
-
-#!/bin/bash
-
-# DCMP (Debian Caddy MariaDB PHP) Installation Script
-
-# Set colors for better visualization
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
-# Set locale
-echo -e "${YELLOW}Setting locale...${NC}"
-sudo locale-gen en_US.UTF-8
-sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
-
-# Set timezone
-echo -e "${YELLOW}Setting timezone...${NC}"
-sudo timedatectl set-timezone Asia/Ho_Chi_Minh
-
-# Set nameserver
-echo -e "${YELLOW}Setting Google and Cloudflare nameservers...${NC}"
-echo -e "nameserver 8.8.8.8\nnameserver 1.1.1.1" | sudo tee /etc/resolv.conf
-
-# Enable TCP BBR congestion control
-echo -e "${YELLOW}Enabling TCP BBR congestion control...${NC}"
-cat <<EOF | sudo tee /etc/sysctl.conf
-# TCP BBR congestion control
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
-EOF
-
-# Create swap
-echo -e "${YELLOW}Creating a 2GB swap file...${NC}"
-sudo fallocate -l 2G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-echo 'vm.swappiness=10' | sudo tee /etc/sysctl.d/99-xs-swappiness.conf
-
-# Install Caddy
-echo -e "${YELLOW}Installing Caddy...${NC}"
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy -y
-
-# Prompt user for domain and email
-read -p "$(echo -e ${YELLOW}"[?] Your domain (e.g., example.com): "${NC})" DOMAIN
-read -p "$(echo -e ${YELLOW}"[?] Your email for SSL certificate: "${NC})" EMAIL
 
 # Download Caddyfile from GitHub
 CADDYFILE_URL="https://raw.githubusercontent.com/zonprox/dcmpw/main/Caddyfile"
@@ -172,9 +117,6 @@ sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
 sed -i "s/localhost/127.0.0.1/" wp-config.php
 
-# Remove the downloaded WordPress archive
-rm -f /var/www/$DOMAIN/latest.tar.gz
-
 # Start Caddy
 echo -e "${YELLOW}Starting Caddy...${NC}"
 sudo systemctl start caddy
@@ -183,7 +125,6 @@ sudo systemctl start caddy
 echo -e "${GREEN}Installation and configuration completed.${NC}"
 echo -e "${YELLOW}Configuration Information:${NC}"
 echo -e "Domain: ${GREEN}$DOMAIN${NC}"
-echo -e "Email: ${GREEN}$EMAIL${NC}"
 echo -e "${YELLOW}MariaDB Information:${NC}"
 echo -e "Root Database Password: ${GREEN}$MYSQL_ROOT_PASSWORD${NC}"
 echo -e "Database Name: ${GREEN}$DB_NAME${NC}"
@@ -192,3 +133,4 @@ echo -e "Database Password: ${GREEN}$DB_PASSWORD${NC}"
 
 # Clean up
 echo -e "${YELLOW}Cleaning up...${NC}"
+rm -f /var/www/$DOMAIN/latest.tar.gz
