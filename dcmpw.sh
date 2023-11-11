c9a909ad233509e8441feaeb4d5cb2df9ee42d5b
Zon Harris <zonprox@users.noreply.github.com>
2023-11-11 14:23:12 +0700

    Update dcmpw.sh

--- dcmpw.sh
+++ dcmpw.sh
@@ -49,52 +49,65 @@ sudo apt install caddy -y
 read -p "$(echo -e ${YELLOW}"[?] Your domain (e.g., example.com): "${NC})" DOMAIN
 read -p "$(echo -e ${YELLOW}"[?] Your email for SSL certificate: "${NC})" EMAIL
 
-# Configure Caddyfile
-echo -e "${YELLOW}Configuring Caddyfile...${NC}"
-echo -e "${GREEN}$DOMAIN${NC} {
-    root * /var/www/$DOMAIN/wordpress
-    encode zstd gzip
+#!/bin/bash
 
-    @disallowed {
-        path /xmlrpc.php
-        path /wp-content/uploads/*.php
-    }
+# DCMP (Debian Caddy MariaDB PHP) Installation Script
 
-    rewrite @disallowed /index.php
+# Set colors for better visualization
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
-    # Serve a PHP site through php-fpm
-    php_fastcgi unix//run/php/php7.4-fpm.sock
+# Set locale
+echo -e "${YELLOW}Setting locale...${NC}"
+sudo locale-gen en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
 
-    @static {
-        file
-        path *.css *.js *.ico *.woff *.woff2
-    }
-    handle @static {
-        header Cache-Control \"public, max-age=31536000\"
-    }
+# Set timezone
+echo -e "${YELLOW}Setting timezone...${NC}"
+sudo timedatectl set-timezone Asia/Ho_Chi_Minh
 
-    @static-img {
-        file
-        path *.gif *.jpg *.jpeg *.png *.svg *.webp *.avif
-    }
-    handle @static-img {
-        header Cache-Control \"public, max-age=31536000, immutable\"
-    }
+# Set nameserver
+echo -e "${YELLOW}Setting Google and Cloudflare nameservers...${NC}"
+echo -e "nameserver 8.8.8.8\nnameserver 1.1.1.1" | sudo tee /etc/resolv.conf
 
-    # Enable the static file server.
-    file_server {
-        precompressed zstd gzip
-        index index.html
-    }
+# Enable TCP BBR congestion control
+echo -e "${YELLOW}Enabling TCP BBR congestion control...${NC}"
+cat <<EOF | sudo tee /etc/sysctl.conf
+# TCP BBR congestion control
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+EOF
 
-    log {
-        output file /var/log/caddy/ssl_access.log {
-            roll_size 100mb
-            roll_keep 3
-            roll_keep_for 7d
-        }
-    }
-}" | sudo tee /etc/caddy/Caddyfile
+# Create swap
+echo -e "${YELLOW}Creating a 2GB swap file...${NC}"
+sudo fallocate -l 2G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo 'vm.swappiness=10' | sudo tee /etc/sysctl.d/99-xs-swappiness.conf
+
+# Install Caddy
+echo -e "${YELLOW}Installing Caddy...${NC}"
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy -y
+
+# Prompt user for domain and email
+read -p "$(echo -e ${YELLOW}"[?] Your domain (e.g., example.com): "${NC})" DOMAIN
+read -p "$(echo -e ${YELLOW}"[?] Your email for SSL certificate: "${NC})" EMAIL
+
+# Download Caddyfile from GitHub
+CADDYFILE_URL="https://raw.githubusercontent.com/zonprox/dcmpw/main/Caddyfile"
+echo -e "${YELLOW}Downloading Caddyfile...${NC}"
+sudo curl -sSL "$CADDYFILE_URL" -o /etc/caddy/Caddyfile
+
+# Update Caddyfile with user's domain
+echo -e "${YELLOW}Updating Caddyfile with your domain...${NC}"
+sudo sed -i "s/example.com/$DOMAIN/g" /etc/caddy/Caddyfile
 
 # Install PHP extensions
 echo -e "${YELLOW}Installing PHP extensions${NC}"
